e23081a3b51c2b75a252e1dc29fe35b0e4c983f7
noel-schneider <noel@mob-box.eu>
2023-12-28 19:12:34 +0100

    :recycle: Call the two main components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Form from "./Form";
 import Graph from "./Graph";
 
 function App() {
 
+    const [selectedNodes, setSelectedNodes] = useState([]);
+
+    const updateSelectedNodes = (nodes) => {
+        setSelectedNodes(nodes);
+    };
+
   return (
       <>
-        <Form/>
-        <Graph/>
+          <Form selectedNodes={selectedNodes} />
+          <Graph updateSelectedNodes={updateSelectedNodes} />
       </>
   );
 }
