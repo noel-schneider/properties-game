d7bd650d5ea8ad888283ba48376740929ed06a31
noel-schneider <noel@mob-box.eu>
2023-12-28 18:56:02 +0100

    :sparkles: Define input to enter the categories

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./Form.css"
 
-function Form() {
+function Form({selectedNodes}) {
+
+    const [inputValue, setInputValue] = React.useState("");
+    const [isSubmitEnabled, setIsSubmitEnabled] = React.useState(false);
+
+    useEffect(() => {
+        setIsSubmitEnabled(selectedNodes.length >= 3 && inputValue.length > 0);
+    }, [inputValue, selectedNodes]);
+
+  const handleSubmit = () => {
+    if (isSubmitEnabled) {
+      console.log("Submit button clicked!");
+    }
+  };
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setInputValue(e.target.value);
+    }
+
     return (
         <div className="input-container">
-            <input className="input" type="text" placeholder='Type a category here!'/>
-            <button className="submit" type="submit">
+            <input id="category-input" className="input" type="text" onChange={(e) => handleChange(e)} placeholder='Type a category here!'/>
+            <button className="submit" type="submit" onClick={handleSubmit} disabled={!isSubmitEnabled}>
                 Submit
             </button>
         </div>
