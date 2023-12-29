c06cb40250461327dd5d076d82c0d66f8fa988e1
noel-schneider <noel@mob-box.eu>
2023-12-29 22:59:22 +0100

    :sparkles: Add indication to press enter

--- src/Form.js
+++ src/Form.js
@@ -23,10 +23,17 @@ function Form({selectedNodes}) {
 
     return (
         <div className="input-container">
-            <input id="category-input" className="input" type="text" onChange={(e) => handleChange(e)} placeholder='Type a category here!'/>
-            <button className="submit" type="submit" onClick={handleSubmit} disabled={!isSubmitEnabled}>
-                Submit
-            </button>
+            <div className="input-and-submit-container">
+                <input id="category-input" className="input" type="text" onChange={(e) => handleChange(e)}
+                       placeholder='Type a category here!'/>
+                <button className="submit" type="submit" onClick={handleSubmit} disabled={!isSubmitEnabled}>
+                    Submit
+                </button>
+            </div>
+            <div className="press-enter-wrapper">
+                <img className={"enter-key-image"} src={process.env.PUBLIC_URL + "/enter-key.png"} alt={'Press enter'}/>
+                <p className={'small-text'}>Press 'Enter' to select!</p>
+            </div>
         </div>
     );
 }
